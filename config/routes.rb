50d6f800e3add4fff7fa1dd38aa0dcd85053acb4
Millene Prado <millene_vprado@hotmail.com>
2022-08-16 11:11:53 -0300

    restaurant crud - 7 routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # verb 'path', to: 'controller#action'
+  # Read all restaurants
+  get 'restaurants', to: 'restaurants#index'
+
+  # Create a restaurant
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+
+  # Read one restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Update a restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch 'restaurants/:id', to: 'restaurants#update'
+
+  # Delete a restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # RESOURCES
+  # resources :restaurants, except: [:destroy]
+  # resources :restaurants, only: [:index, :show]
 end
